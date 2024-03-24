30c00bebb5a2ae51b28b63b730034bcd59b95fc2
21Staceye <Staceyerin5@gmail.com>
2024-03-23 22:09:45 -0400

    order screen updates

---
+++ Scripts/Order/customer.gd
@@ -0,0 +1,20 @@
+extends Node2D
+var counterr = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if counterr == false:
+		translate(Vector2(-delta*45, 0))
+	else:
+		pass
+
+
+func _on_area_2d_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	print("owey")
+	print(area_shape_index)
+	counterr=true
---
+++ Scripts/Order/order.gd
@@ -0,0 +1,35 @@
+extends Area2D
+
+
+var counterr = false
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if counterr == false:
+		translate(Vector2(-delta*45, 0))
+	else:
+		pass
+	
+
+
+
+
+
+
+
+
+
+
+func _on_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	print(area_shape_index)
+	counterr = true 
+	if(area_shape_index == 0):
+		$/root/Order_screen/Control/Order_button.visible = true
+	
+
+
+
