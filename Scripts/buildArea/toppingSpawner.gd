97eef1d673869c7e904fa0e124f5583693716403
HannahKnisley <122892305+HannahKnisley@users.noreply.github.com>
2024-03-24 23:07:13 -0400

    update title screen and project settings

--- Scripts/buildArea/toppingSpawner.gd
+++ Scripts/buildArea/toppingSpawner.gd
@@ -1,12 +1,12 @@
 extends Area2D
 
-@export var patty: PackedScene
+@export var topping: PackedScene
 
 var canSpawn = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	spawn_patty()
+	spawn_item()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,21 +14,21 @@ func _process(delta):
 	if Input.is_action_just_released("mouseClick"):
 		await get_tree().create_timer(0.1).timeout
 		if canSpawn:
-			spawn_patty()
+			spawn_item()
 
 
-func spawn_patty():
-	var newPatty = patty.instantiate()
-	self.add_child(newPatty)
-	newPatty.global_position = self.global_position
+func spawn_item():
+	var newTopping = topping.instantiate()
+	self.add_child(newTopping)
+	newTopping.global_position = self.global_position
 	
 
 
 func _on_body_entered(body):
-	if body.is_in_group("patty"):
+	if body.is_in_group("topping"):
 		canSpawn = false
 
 
 func _on_body_exited(body):
-	if body.is_in_group("patty"):
+	if body.is_in_group("topping"):
 		canSpawn = true
