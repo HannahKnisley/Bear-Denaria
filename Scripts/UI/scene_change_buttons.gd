0f7f75561125a5290d38a8c1dc1fcdadac2f0d1c
HannahKnisley <122892305+HannahKnisley@users.noreply.github.com>
2024-03-25 11:46:21 -0400

    made window smaller

--- Scripts/UI/scene_change_buttons.gd
+++ Scripts/UI/scene_change_buttons.gd
@@ -16,7 +16,7 @@ func _on_order_button_pressed():
 
 
 func _on_grill_button_pressed():
-	$/root/WorldRoot/Camera.global_position = $/root/WorldRoot/GrillArea.global_position + Vector2(1000,700)
+	$/root/WorldRoot/Camera.global_position = $/root/WorldRoot/GrillArea.global_position + Vector2(500,350)
 	$/root/WorldRoot/CanvasLayer/TakingOrderButton.visible = false
 	$/root/WorldRoot/Order_screen/Order_button.visible = false
 
