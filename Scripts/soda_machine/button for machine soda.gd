042305d54dd5cac0250aa08f8f62444aeda22a35
21Staceye <Staceyerin5@gmail.com>
2024-03-27 13:03:49 -0400

    didsomesodawork

--- Scripts/soda_machine/button for machine soda.gd
+++ Scripts/soda_machine/button for machine soda.gd
@@ -2,9 +2,12 @@ extends Area2D
 var mouseEnerted = false
 var followMouse = false
 var mouseCollide = false
+var defaultPosition
+var snapPos
+var cupEntered = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	defaultPosition = self.position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,7 +15,7 @@ func _process(delta):
 	if Input.is_action_just_pressed("mouseClick") and mouseEnerted:
 		followMouse = true
 	if Input.is_action_pressed("mouseClick") and followMouse:
-		self.global_position = get_local_mouse_position()
+		self.position = get_global_mouse_position()
 		
 	if Input.is_action_just_released("mouseClick"):
 		followMouse = false
@@ -28,4 +31,5 @@ func _on_mouse_exited():
 
 
 func _on_plate_for_cup_mouse_entered():
-	print("yo")
+	cupEntered = true
+
